490b5fb88e2f744f2a63f0e84cdafdddce42795c
Efren Castillo <efrencast@gmail.com>
2018-12-05 22:15:09 -0500

    dom scripting part 2: completed

--- app.js
+++ app.js
@@ -1,64 +1,63 @@
-  function onReady() {
-
-  // array of to-dos
+function onReady() {
   const toDos = [];
-  const addToDoForm = document.getElementById('addToDoForm'); // this line access html form
-
-    // this function updates the to-dos arrays
-    function createNewToDo() {
-
-    // access the text input to get the text people enter
-    const newToDoText = document.getElementById('newToDoText');
-
-    // prevent user to submit empty to-do items
-      if (!newToDoText.value) { return; }
-
-    // add the new to-do to the array
-    toDos.push({
-      title: newToDoText.value,
-      complete: false
-    });
-
-    // clear text input (courtesy to the user :-)
-    newToDoText.value = '';
-
-    // renderTheUI from createNewToDo
-    renderTheUI(); {
-
-
-    // event listerner
-    addToDoForm.addEventListener('submit', event => {
-      event.preventDefault(); // prevents page from reloading
-      createNewToDo();
+  const addToDoForm = document.getElementById('addToDoForm');
+function createNewToDo() {
+  const newToDoText = document.getElementById('newToDoText');
+  if (!newToDoText.value) { return; }
+  toDos.push({
+    title: newToDoText.value,
+    complete: false
   });
-}
-  //function to create the ui
-    renderTheUI();
-      const toDolist = document.getElementById('toDoList'); // access <ul> in html
+  newToDoText.value = '';
 
-      // set <newLi> to an empty string
-      toDoList.textContent = '';
+  renderTheUI();
+} // end of function createNewToDo
 
-      // array method to render each to-do as <li> in the <ul>
-      toDos.forEach(function(toDo) {
+function renderTheUI() {
+  const toDoList = document.getElementById('toDoList');
 
-        // <li> and checkbox
-        const newLi = document.createElement('li');
-        const checkbox = document.createElement('input');
-        checkbox.type = "checkbox"; // tells the 'input' what kind
+  toDoList.textContent = '';
 
-        // add title text to newLi
-        newLi.textContent = toDo.title;
+  toDos.forEach(function(toDo){
+    const newLi = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
 
-        // update the dom
-        toDoList.appendChild(newLi);
-        newLi.appendChild(checkbox);
-      });
+    newLi.textContent = toDo.title;
 
-    }
+    toDoList.appendChild(newLi);
+    newLi.appendChild(checkbox);
+  });
+} // end of function renderTheUI
+
+addToDoForm.addEventListener('submit', event => {
+  event.preventDefault();
+  createNewToDo();
+});
+renderTheUI();
+} // end of funtion onReady
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// last function
   window.onload = function() {
     onReady();
 };
