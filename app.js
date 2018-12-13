9e6053eeae19f30c31527a75d8e6e6895c089a43
Efren Castillo <efrencast@gmail.com>
2018-12-12 22:46:43 -0500

    dom-scripting-part-2-assignment

--- app.js
+++ app.js
@@ -1,54 +1,60 @@
-function onReady() {
-  const toDos = [];
+function onReady(){
+  let id = 0;
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm'); // access the html form
 
   //function createNewToDo
-function createNewToDo() {
-  const newToDoText = document.getElementById('newToDoText');
-  if (!newToDoText.value) { return; } // prevents submit empty
-  toDos.push({
-    title: newToDoText.value,
-    complete: false
-  });
-  newToDoText.value = ''; // clears text for the user
+  function createNewToDo(){
+    const newToDoText = document.getElementById('newToDoText');
+    toDos.push({
+      title: newToDoText.value,
+      complete: false,
+      id: id
+    });
+    newToDoText.value = ''; // clears text for the user
+    id++;
 
-  renderTheUI(); // when state changes ui gets updated
-}
-  // end of function createNewToDo
+    renderTheUI(); // when state changes ui gets updated
+  }
 
- // function to render the ui
-function renderTheUI() {
-  const toDoList = document.getElementById('toDoList'); // access the <ul> in html
+  // function to render the ui
+  function renderTheUI(){
+    const toDoList = document.getElementById('toDoList');
+    toDoList.textContent = ''; // set newLi to an empty string
 
-  toDoList.textContent = ''; // set newLi to an empty string
+    toDos.forEach(function(toDo){ // takes function & applies it to each item in array
+      const newLi = document.createElement('li'); // creates ui for li and checkbox
+      let checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
 
-  toDos.forEach(function(toDo){ // takes function & applies it to each item in array
-    const newLi = document.createElement('li'); // creates ui for li and checkbox
-    const checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
+      newLi.textContent = toDo.title; // add the toDo title text to newLi
 
-    newLi.textContent = toDo.title; // add the toDo title text to newLi
+        // delete button
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
 
-    toDoList.appendChild(newLi); // updated dom
-    newLi.appendChild(checkbox); // updated dom
-  });
-} // end of function renderTheUI
+        // event listener
+        deleteButton.addEventListener('click',()  => {
+          toDos = toDos.filter(toDos => toDo.id != toDos.id);
+          renderTheUI();
+        })
 
+        toDoList.appendChild(newLi); // updated dom with li
+        newLi.appendChild(checkbox); // updated dom with checkbox
+        newLi.appendChild(deleteButton); // updated dom with delete button
+
+    });
+
+  }
 
   // event listener
-addToDoForm.addEventListener('submit', event => {
-  event.preventDefault(); // prevents page reload
-  createNewToDo();
-});
- // end event listener
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+  })
 
- // function to CALL the render the ui
-renderTheUI();
+};
 
-
-} // end of funtion onReady
-
-// last function
-  window.onload = function() {
-    onReady();
+window.onload = function() {
+  onReady();
 };
